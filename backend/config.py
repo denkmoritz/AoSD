8a19e5c1dea681b16fd6984c1d81c59fcb12e9c9
Moritz <moritzdenk0407@gmail.com>
2025-03-08 10:09:24 +0100

    Delete backend/config.py

--- backend/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    api_key_openaq = os.getenv("api_key_open_aq", "bd4abee7cfa6bc34df460759e71b2a5834ead20542afd4231f4e642409bb143e")
-    api_key_openweather = os.getenv("api_key_openweather", "d936429564d5be4cd5b1e1545b3fe100")
